791f1cca83f8b95aa1216675407267f33a352434
Fachri Hawari <fachri.hawari@gmail.com>
2024-09-07 10:07:56 +0700

    split placeholder to separate file

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,6 +2,7 @@ import Carousel from "@/components/Carousel";
 import SectionProducts from "@/components/SectionProducts";
 import { getLanding, getBestSellers } from "@/actions/products";
 import { Suspense } from "react";
+import SectionProductsPlaceholder from "@/components/SectionProductsPlaceholder";
 
 export default async function Home() {
   return (
@@ -14,18 +15,5 @@ export default async function Home() {
         <SectionProducts label="Best Sellers" backgroundColor="bg-purple-100" getProducts={getBestSellers} />
       </Suspense>
     </>
-  );
-}
-
-function SectionProductsPlaceholder() {
-  return (
-    <div className="animate-pulse py-8 container mx-auto px-4">
-      <div className="h-8 bg-gray-200 rounded w-1/6 mb-4"></div>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
-        {[...Array(5)].map((_, index) => (
-          <div key={index} className="bg-gray-200 h-64 rounded"></div>
-        ))}
-      </div>
-    </div>
   );
 }
---
+++ src/components/SectionProductsPlaceholder.tsx
@@ -0,0 +1,16 @@
+
+
+function SectionProductsPlaceholder() {
+  return (
+    <div className="animate-pulse py-8 container mx-auto px-4">
+      <div className="h-8 bg-gray-200 rounded w-1/6 mb-4"></div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+        {[...Array(5)].map((_, index) => (
+          <div key={index} className="bg-gray-200 h-64 rounded"></div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default SectionProductsPlaceholder;
