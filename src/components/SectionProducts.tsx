889b2fe0dffc46f7fe59dfa5cca0547e4230a586
Fachri Hawari <fachri.hawari@gmail.com>
2024-09-08 08:42:12 +0700

    remove action

--- src/components/SectionProducts.tsx
+++ src/components/SectionProducts.tsx
@@ -17,10 +17,6 @@ async function SectionProducts({ label, backgroundColor = 'bg-white', getProduct
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between mb-6">
           <h2 className="text-2xl font-bold text-gray-800">{label}</h2>
-          <div className="flex items-center text-green-500">
-            <span>See All</span>
-            <FiArrowRight className="ml-2" />
-          </div>
         </div>
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
           {products.map((product) => (
