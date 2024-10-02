c835191f23987ede840fbc926b7da38c888654e9
Fachri Hawari <fachri.hawari@gmail.com>
2024-10-02 17:37:04 +0700

    remove unused font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,12 @@
 import SweetAlert from "@/components/SweetAlert";
 import "./globals.css";
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
 import { Suspense } from "react";
 
 export default function RootLayout({ children }: Readonly<Props>) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} font-sans`}>
+      <body className='font-sans'>
         <Suspense>
           <SweetAlert />
         </Suspense>
@@ -16,12 +15,6 @@ export default function RootLayout({ children }: Readonly<Props>) {
     </html>
   );
 }
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  variable: '--font-poppins',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900',]
-});
 
 export const metadata: Metadata = {
   title: "tokopakedi",
